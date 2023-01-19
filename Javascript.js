ea2ff79538c2e76a872325f9c13adb39915d792b
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-01-18 21:12:11 -0600

    Add html,javascript,css

--- Javascript.js
+++ Javascript.js
@@ -19,73 +19,96 @@ function playRound(playerSelection,computerSelection){
 
     let result;
     let verdict;
+    let roundresult = document.querySelector('.roundresult');
 
-    if (playerSelection.toUpperCase() == computerSelection.toUpperCase()){
+    if (playerSelection === computerSelection){
         result = "Game Draw";
         verdict = 'Game Draw';
-    }else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection.toUpperCase() =='PAPER'){
-        result = 'You Lose! Paper beats Rock';
+    }else if (playerSelection == 'ROCK' && computerSelection =='PAPER'){
+        result = 'You Lost this round! Paper beats Rock';
         verdict = 'Lost';
-    }else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection.toUpperCase() =='SCISSOR'){
-        result = 'You Win! Rock beats Scissor';
+    }else if (playerSelection == 'ROCK' && computerSelection =='SCISSOR'){
+        result = 'You Won this round! Rock beats Scissor';
         verdict = 'Won';
-    }else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() =='ROCK'){
-        result = 'You Win! Paper beats Rock';
+    }else if (playerSelection == 'PAPER' && computerSelection =='ROCK'){
+        result = 'You Won this round! Paper beats Rock';
         verdict = 'Won';
-    }else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() =='SCISSOR'){
-        result = 'You Lose! Scissor beats Paper';
+    }else if (playerSelection == 'PAPER' && computerSelection =='SCISSOR'){
+        result = 'You Lost this round! Scissor beats Paper';
         verdict = 'Lost';
-    }else if (playerSelection.toUpperCase() == 'SCISSOR' && computerSelection.toUpperCase() =='ROCK'){
-        result = 'You Lose! Rock beats Scissor';
+    }else if (playerSelection == 'SCISSOR' && computerSelection =='ROCK'){
+        result = 'You Lost this round! Rock beats Scissor';
         verdict = 'Lost';
-    }else if (playerSelection.toUpperCase() == 'SCISSOR' && computerSelection.toUpperCase() =='PAPER'){
-        result = 'You Win! Scissor beats Paper';
+    }else if (playerSelection == 'SCISSOR' && computerSelection =='PAPER'){
+        result = 'You Won this round! Scissor beats Paper';
         verdict = 'Won';
     }
 
-    console.log(result);
+    roundresult.textContent = `${result}`;
     return verdict;
 }
 
-function game(){
 
-    let playerScore = 0 ;
-    let computerScore = 0 ;
-
-
-    for (let i = 0; i <5 ; i++){
-
-        let playerSelection = prompt("Please enter your choice in Rock,Paper,Scissor");
-
-        let computerSelection = getComputerChoice();
-
-        let verdict = playRound(playerSelection,computerSelection);
-
-        if (verdict === 'Won'){
-            playerScore += 1;
-        } else if (verdict === 'Lost'){
-            computerScore += 1;
-        }
-        
-
-    }
-
-    console.log('Player : ' + playerScore);
-    console.log('Computer : ' + computerScore);
-    
-    whoWon(playerScore, computerScore);
-
-}
 
 function whoWon(playerScore, computerScore){
-    if (playerScore > computerScore){
-        console.log("You won");
-    }else if (playerScore < computerScore){
-        console.log('You Lost');
+    if (playerScore == 5){
+        let finalresult = document.querySelector('.roundresult');
+        finalresult.textContent = 'YOU WON THE GAME :) HIT RESTART TO PLAY AGAIN';        
+    }else if (computerScore == 5){
+        let finalresult = document.querySelector('.roundresult');
+        finalresult.textContent = 'YOU LOST THE GAME :( HIT RESTART TO PLAY AGAIN';
     }else {
-        console.log('Game Draw')
+        return;
     }
 }
-game();
 
 
+let playerScore = 0 ;
+
+let computerScore = 0 ; 
+
+let playerSelection = '';  
+
+const player_Score = document.querySelector('.playerscore');
+
+const computer_Score = document.querySelector('.computerscore');
+
+let roundresult = document.querySelector('.roundresult');
+roundresult.textContent = 'Start game';
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        if (playerScore <5 && computerScore <5){
+            playerSelection = button.id;
+
+            let computerSelection = getComputerChoice();  
+
+            let verdict = playRound(playerSelection,computerSelection);
+
+            if (verdict == 'Won'){
+                playerScore += 1;
+            }else if (verdict == 'Lost'){
+                computerScore += 1;
+            }
+
+            player_Score.textContent = `Player Score : ${playerScore}`;
+            computer_Score.textContent = `Computer Score: ${computerScore}`;
+                
+            whoWon(playerScore, computerScore);
+        }
+
+    });
+});
+
+const replay = document.querySelector('.replay');
+
+replay.addEventListener('click',() =>{
+    playerScore = 0 ;
+    computerScore = 0 ; 
+    player_Score.textContent = `Player Score : ${playerScore}`;
+    computer_Score.textContent = `Computer Score: ${computerScore}`;
+    roundresult.textContent = 'Start game';
+});
